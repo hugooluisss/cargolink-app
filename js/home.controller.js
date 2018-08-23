d760f2c797f5e5a07e9162cd4f192c09ae21e416
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2018-08-23 11:47:42 -0500

    Modificaciones al Login

--- js/home.controller.js
+++ js/home.controller.js
@@ -3,4 +3,17 @@ function callHome(){
 	$("#modulo").html(plantillas["home"]);
 	setPanel();
 	console.info("Carga de home finalizada");
+	
+	$("#btnSalir").click(function(){
+		alertify.confirm("¿Seguro?", function(e){
+    		if(e) {
+    			window.plugins.PushbotsPlugin.removeTags(["chofer", "operador"]);
+    			window.plugins.PushbotsPlugin.removeAlias();
+	    		window.localStorage.removeItem("sesion");
+	    		window.localStorage.removeItem("idOrden");
+	    		//backgroundGeolocation.stop();
+	    		location.href = "index.html";
+	    	}
+    	});
+	});
 }
